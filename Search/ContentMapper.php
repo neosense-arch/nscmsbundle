96164c26f7ae99a87e619124d61f1665ba53a8c0
Alexey Gromov <italianwind@gmail.com>
2013-02-08 01:34:03 +0400

    NEW Added empty search mapper and repository

---
+++ Search/ContentMapper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NS\CmsBundle\Search;
+
+use NS\SearchBundle\Agent\MapperInterface;
+
+class ContentMapper implements MapperInterface
+{
+}
---
+++ Search/ContentRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NS\CmsBundle\Search;
+
+use NS\SearchBundle\Agent\RepositoryInterface;
+
+class ContentRepository implements RepositoryInterface
+{
+}
