ec72bb884b08a8e33439ad83a19960ba2ee69871
Alexey Gromov <italianwind@gmail.com>
2014-02-14 14:26:47 +0400

    NEW Added bundle title

--- NSCmsBundle.php
+++ NSCmsBundle.php
@@ -2,8 +2,18 @@
 
 namespace NS\CmsBundle;
 
+use NS\CoreBundle\Bundle\CoreBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class NSCmsBundle extends Bundle
+class NSCmsBundle extends Bundle implements CoreBundle
 {
+    /**
+     * Retrieves human-readable bundle title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return 'Системные';
+    }
 }
